3a11bf8f2e0cae4d3756843affaf33ce8c382787
pedro appipe <pedroappipe@live.com>
2025-05-17 15:06:28 -0300

    commit inicial

---
+++ portal-aluno/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'appito';
+}
+// componente {
+//   um arquivo typescript
+//   um arquivo html
+//   um arquivo css
+// }
